cee08b167ca8f39fbb3c900c07f44d00012a14de
eolender <hyouhakusha@gmail.com>
2023-08-22 22:42:47 +0200

    Return Error when unsupported media type in header request

--- src/main/java/com/task/githubviewer/rest/ViewerController.java
+++ src/main/java/com/task/githubviewer/rest/ViewerController.java
@@ -1,8 +1,11 @@
 package com.task.githubviewer.rest;
 
 import com.task.githubviewer.github.GithubRepo;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +15,12 @@ import java.util.List;
 @RequestMapping("/api/viewer")
 public class ViewerController {
 
-    @GetMapping("/{username}")
-    ResponseEntity<List<GithubRepo>> getAllUserGithubRepos() {
+    @GetMapping(value = "/{username}")
+    ResponseEntity<List<GithubRepo>> getAllUserGithubRepos(@RequestHeader(HttpHeaders.ACCEPT) String acceptHeader) {
+        if (!acceptHeader.equals(MediaType.APPLICATION_JSON_VALUE)) {
+            throw new NotAcceptableException("Unsupported media type: " + acceptHeader + " not accepted");
+        }
+
         return  null;
     }
 
