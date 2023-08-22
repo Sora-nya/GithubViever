c2a40ec876b8224d4daffb4b6d7dbd1dd7e6afca
eolender <hyouhakusha@gmail.com>
2023-08-22 21:16:58 +0200

    Initial commit

---
+++ src/test/java/com/task/githubviewer/GithubViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.githubviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubViewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
